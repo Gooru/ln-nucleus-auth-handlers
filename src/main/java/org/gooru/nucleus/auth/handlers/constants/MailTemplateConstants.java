2caa69e05ff96fb921c5bad914fdfd597cb48a7f
szgooru <sachin@gooru.org>
2016-08-13 03:46:27 +0530

    GG-1789: verify user by canonical username. Fixing email template name for password changed confirmation

--- src/main/java/org/gooru/nucleus/auth/handlers/constants/MailTemplateConstants.java
+++ src/main/java/org/gooru/nucleus/auth/handlers/constants/MailTemplateConstants.java
@@ -4,7 +4,7 @@ public final class MailTemplateConstants {
     public static final String WELCOME_MAIL = "welcome_mail";
     public static final String PARENT_REGISTARTION_CONFIRMATION = "parent_registration_confirmation";
     public static final String INVITE_COLLABORATOR = "invite_collaborator";
-    public static final String PASSWORD_CHANGED = "password_changed";
+    public static final String PASSWORD_CHANGED = "password_changed_confirmation";
     public static final String EMAIL_ADDRESS_CHANGE_REQUEST = "email_address_change_request";
     public static final String USER_REGISTARTION_CONFIRMATION = "user_registration_confirmation";
     public static final String CHILD_USER_REGISTARTION_CONFIRMATION = "child_user_registration_confirmation";
