3d93f408d0156a1ca92aeb64b213b70f6b12a6b8
szgooru <sachin@gooru.org>
2017-02-07 23:53:25 +0530

    fix for the update user api, conversion of the fields to proper type

--- src/main/java/org/gooru/nucleus/auth/handlers/processors/repositories/activejdbc/entities/AJEntityUsers.java
+++ src/main/java/org/gooru/nucleus/auth/handlers/processors/repositories/activejdbc/entities/AJEntityUsers.java
@@ -141,9 +141,12 @@ public class AJEntityUsers extends Model {
         converterMap.put(TENANT_ID, (fieldValue -> FieldConverter.convertFieldToUuid((String) fieldValue)));
         converterMap.put(BIRTH_DATE,
             (fieldValue -> FieldConverter.convertFieldToDateWithFormat(fieldValue, DateTimeFormatter.ISO_LOCAL_DATE)));
-        converterMap.put(PASSWORD, (FieldConverter::convertPasswordToEncryted));
-        converterMap.put(USERNAME, (FieldConverter::convertFieldToLowercase));
-        converterMap.put(EMAIL, (FieldConverter::convertFieldToLowercase));
+        converterMap.put(PASSWORD, (fieldValue -> FieldConverter.convertPasswordToEncryted(fieldValue)));
+        converterMap.put(USERNAME, (fieldValue -> FieldConverter.convertFieldToLowercase(fieldValue)));
+        converterMap.put(EMAIL, (fieldValue -> FieldConverter.convertFieldToLowercase(fieldValue)));
+        converterMap.put(GRADE, (fieldValue -> FieldConverter.convertFieldToJson(fieldValue)));
+        converterMap.put(COUNTRY_ID, (fieldValue -> FieldConverter.convertFieldToUuid((String) fieldValue)));
+        converterMap.put(STATE_ID, (fieldValue -> FieldConverter.convertFieldToUuid((String) fieldValue)));
         return converterMap;
     }
 
