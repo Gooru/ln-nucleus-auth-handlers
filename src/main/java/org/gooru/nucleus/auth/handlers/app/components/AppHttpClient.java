c2058ac48dd27d4e7e5e869286b291312d8feb22
szgooru <sachin@gooru.org>
2017-09-04 12:42:22 +0530

    making http client component singleton

--- src/main/java/org/gooru/nucleus/auth/handlers/app/components/AppHttpClient.java
+++ src/main/java/org/gooru/nucleus/auth/handlers/app/components/AppHttpClient.java
@@ -20,18 +20,18 @@ public final class AppHttpClient implements Initializer, Finalizer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AppHttpClient.class);
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(HelperConstants.RESOURCE_BUNDLE);
-
-    private static final AppHttpClient INSTANCE = new AppHttpClient();
+    private volatile boolean initialized = false;
+    
     private HttpClient httpClient;
 
     private static final String KEY_ENDPOINT = "api.endpoint";
     private static final String KEY_HOST = "api.host";
     private static final String KEY_PORT = "api.port";
-    private static final String KEY_MAX_POOLSIZE = "http.conn.max.poolsize";
+    private static final String KEY_MAX_POOLSIZE = "http.conn.poolsize";
     private static final String KEY_EVENT_CONFIG = "event.config";
 
     private static final int DEFAULT_PORT = 8080;
-    private static final int DEFAULT_MAX_POOLSIZE = 20;
+    private static final int DEFAULT_POOLSIZE = 20;
 
     private String host;
     private int port;
@@ -42,37 +42,45 @@ public final class AppHttpClient implements Initializer, Finalizer {
     }
 
     public static AppHttpClient getInstance() {
-        return INSTANCE;
+        return Holder.INSTANCE;
     }
 
     @Override
     public void initializeComponent(Vertx vertx, JsonObject config) {
+        LOGGER.debug("Initializing called for http client");
+        if (!initialized) {
+            LOGGER.debug("may have to initialize http client");
+            synchronized (Holder.INSTANCE) {
+                if (!initialized) {
+                    LOGGER.debug("initializing http client after double checking");
+                    JsonObject eventConfig = config.getJsonObject(KEY_EVENT_CONFIG);
+                    if (eventConfig == null || eventConfig.isEmpty()) {
+                        LOGGER.warn(RESOURCE_BUNDLE.getString("event.config.not.found"));
+                        throw new AssertionError(RESOURCE_BUNDLE.getString("event.config.not.found"));
+                    }
 
-        JsonObject eventConfig = config.getJsonObject(KEY_EVENT_CONFIG);
-        if (eventConfig == null || eventConfig.isEmpty()) {
-            LOGGER.warn(RESOURCE_BUNDLE.getString("event.config.not.found"));
-            throw new AssertionError(RESOURCE_BUNDLE.getString("event.config.not.found"));
+                    this.host = eventConfig.getString(KEY_HOST);
+                    if (this.host == null || this.host.isEmpty()) {
+                        LOGGER.warn(RESOURCE_BUNDLE.getString("api.host.missing"));
+                        throw new AssertionError(RESOURCE_BUNDLE.getString("api.host.missing"));
+                    }
+
+                    this.port = eventConfig.getInteger(KEY_PORT, DEFAULT_PORT);
+                    this.maxPoolsize = eventConfig.getInteger(KEY_MAX_POOLSIZE, DEFAULT_POOLSIZE);
+
+                    this.endpoint = eventConfig.getString(KEY_ENDPOINT);
+                    if (this.endpoint == null || this.endpoint.isEmpty()) {
+                        LOGGER.warn(RESOURCE_BUNDLE.getString("api.endpoint.missing"));
+                        throw new AssertionError(RESOURCE_BUNDLE.getString("api.endpoint.missing"));
+                    }
+
+                    this.httpClient = vertx.createHttpClient(new HttpClientOptions().setDefaultHost(this.host)
+                        .setDefaultPort(this.port).setMaxPoolSize(this.maxPoolsize));
+                    initialized = true;
+                    LOGGER.debug("App Http Client initialized successfully");
+                }
+            }
         }
-
-        this.host = eventConfig.getString(KEY_HOST);
-        if (this.host == null || this.host.isEmpty()) {
-            LOGGER.warn(RESOURCE_BUNDLE.getString("api.host.missing"));
-            throw new AssertionError(RESOURCE_BUNDLE.getString("api.host.missing"));
-        }
-
-        this.port = eventConfig.getInteger(KEY_PORT, DEFAULT_PORT);
-        this.maxPoolsize = eventConfig.getInteger(KEY_MAX_POOLSIZE, DEFAULT_MAX_POOLSIZE);
-
-        this.endpoint = eventConfig.getString(KEY_ENDPOINT);
-        if (this.endpoint == null || this.endpoint.isEmpty()) {
-            LOGGER.warn(RESOURCE_BUNDLE.getString("api.endpoint.missing"));
-            throw new AssertionError(RESOURCE_BUNDLE.getString("api.endpoint.missing"));
-        }
-
-        this.httpClient = vertx.createHttpClient(new HttpClientOptions().setDefaultHost(this.host)
-            .setDefaultPort(this.port).setMaxPoolSize(this.maxPoolsize));
-
-        LOGGER.debug("App Http Client initialized successfully");
     }
 
     public HttpClient getHttpClient() {
@@ -90,5 +98,10 @@ public final class AppHttpClient implements Initializer, Finalizer {
     @Override
     public void finalizeComponent() {
         this.httpClient.close();
+    }
+    
+
+    private static final class Holder {
+        private static final AppHttpClient INSTANCE = new AppHttpClient();
     }
 }
