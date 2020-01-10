aa223311d174825d9ea23ca23c83d89411c26615
ajaykumar2897 <ajaykumar@gooru.org>
2020-01-10 20:24:22 +0530

    Fixed indentation issue

--- src/main/java/org/gooru/nucleus/auth/handlers/processors/commands/UserAccountsProcessor.java
+++ src/main/java/org/gooru/nucleus/auth/handlers/processors/commands/UserAccountsProcessor.java
@@ -9,25 +9,25 @@ import org.slf4j.LoggerFactory;
 
 public class UserAccountsProcessor extends AbstractCommandProcessor {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(UserAccountsProcessor.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserAccountsProcessor.class);
 
-	UserAccountsProcessor(ProcessorContext context) {
-		super(context);
-	}
+    UserAccountsProcessor(ProcessorContext context) {
+        super(context);
+    }
 
-	@Override
-	protected void setDeprecatedVersions() {
-		// NOOP
-	}
+    @Override
+    protected void setDeprecatedVersions() {
+        // NOOP
+    }
 
-	@Override
-	protected MessageResponse processCommand() {
-		try {
-			LOGGER.info("Fetch User accounts");
-			return RepoBuilder.buildUserRepo(context).getUserAccounts();
-		} catch (Throwable t) {
-			LOGGER.error("exception while fetching the accounts details", t);
-			return MessageResponseFactory.createInvalidRequestResponse(t.getMessage());
-		}
-	}
+    @Override
+    protected MessageResponse processCommand() {
+        try {
+            LOGGER.info("Fetch User accounts");
+            return RepoBuilder.buildUserRepo(context).getUserAccounts();
+        } catch (Throwable t) {
+            LOGGER.error("exception while fetching the accounts details", t);
+            return MessageResponseFactory.createInvalidRequestResponse(t.getMessage());
+        }
+    }
 }
