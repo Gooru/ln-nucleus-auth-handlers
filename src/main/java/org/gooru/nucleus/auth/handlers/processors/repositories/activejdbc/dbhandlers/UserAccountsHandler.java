2a49fabdaaa05a2210155bfdf3ca0129558a3d71
ajaykumar2897 <ajaykumar@gooru.org>
2020-01-13 15:25:20 +0530

    resloved comments

--- src/main/java/org/gooru/nucleus/auth/handlers/processors/repositories/activejdbc/dbhandlers/UserAccountsHandler.java
+++ src/main/java/org/gooru/nucleus/auth/handlers/processors/repositories/activejdbc/dbhandlers/UserAccountsHandler.java
@@ -23,55 +23,56 @@ import io.vertx.core.json.JsonObject;
 
 public class UserAccountsHandler implements DBHandler, PayloadValidator {
 
-    private final ProcessorContext context;
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserAccountsHandler.class);
-    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(HelperConstants.RESOURCE_BUNDLE);
+  private final ProcessorContext context;
+  private static final Logger LOGGER = LoggerFactory.getLogger(UserAccountsHandler.class);
+  private static final ResourceBundle RESOURCE_BUNDLE =
+      ResourceBundle.getBundle(HelperConstants.RESOURCE_BUNDLE);
 
-    private String email;
+  private String email;
+  private static final String USER_ACCOUNTS = "user_accounts";
 
-    public UserAccountsHandler(ProcessorContext context) {
-        this.context = context;
+  public UserAccountsHandler(ProcessorContext context) {
+    this.context = context;
+  }
+
+  @Override
+  public ExecutionResult<MessageResponse> checkSanity() {
+    JsonObject errors = validatePayload(context.requestBody(),
+        AJEntityUserAccounts.mandatoryFields(), AJEntityUserAccounts.getValidatorRegistry());
+    if (errors != null && !errors.isEmpty()) {
+      LOGGER.warn("Validation errors for request");
+      return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(errors),
+          ExecutionResult.ExecutionStatus.FAILED);
     }
+    email = context.requestBody().getString(AJEntityUsers.EMAIL);
+    return new ExecutionResult<>(null, ExecutionStatus.CONTINUE_PROCESSING);
+  }
 
-    @Override
-    public ExecutionResult<MessageResponse> checkSanity() {
-        JsonObject errors = validatePayload(context.requestBody(), AJEntityUserAccounts.usersAccountsFieldSelector(),
-            AJEntityUserAccounts.getValidatorRegistry());
-        if (errors != null && !errors.isEmpty()) {
-            LOGGER.warn("Validation errors for request");
-            return new ExecutionResult<>(MessageResponseFactory.createValidationErrorResponse(errors),
-                ExecutionResult.ExecutionStatus.FAILED);
-        }
-        email = context.requestBody().getString(AJEntityUsers.EMAIL);
-        return new ExecutionResult<>(null, ExecutionStatus.CONTINUE_PROCESSING);
+  @Override
+  public ExecutionResult<MessageResponse> validateRequest() {
+    LOGGER.debug("Email: {}", email);
+    LazyList<AJEntityUsers> users = AJEntityUsers.findBySQL(AJEntityUsers.SELECT_BY_EMAIL, email);
+    if (users.isEmpty()) {
+      LOGGER.warn("user not found in database for email: {}", email);
+      return new ExecutionResult<>(MessageResponseFactory.createNotFoundResponse(
+          (RESOURCE_BUNDLE.getString("user.not.found"))), ExecutionStatus.FAILED);
     }
+    return new ExecutionResult<>(null, ExecutionStatus.CONTINUE_PROCESSING);
+  }
 
-    @Override
-    public ExecutionResult<MessageResponse> validateRequest() {
-        LOGGER.debug("Email: {}", email);
-        LazyList<AJEntityUsers> users = AJEntityUsers.findBySQL(AJEntityUsers.SELECT_BY_EMAIL, email);
-        if (users.isEmpty()) {
-            LOGGER.warn("user not found in database for email: {}", email);
-            return new ExecutionResult<>(
-                MessageResponseFactory.createNotFoundResponse((RESOURCE_BUNDLE.getString("user.not.found"))),
-                ExecutionStatus.FAILED);
-        }
-        return new ExecutionResult<>(null, ExecutionStatus.CONTINUE_PROCESSING);
-    }
+  @Override
+  public ExecutionResult<MessageResponse> executeRequest() {
+    List<Map> accounts = Base.findAll(AJEntityUserAccounts.SELECT_ACCOUNTS_BY_EMAIL, email);
+    JsonArray accountsAsJsonArray = new JsonArray(accounts);
+    final JsonObject response = new JsonObject();
+    response.put(USER_ACCOUNTS, accountsAsJsonArray);
+    return new ExecutionResult<>(MessageResponseFactory.createPostResponse(response),
+        ExecutionResult.ExecutionStatus.SUCCESSFUL);
+  }
 
-    @Override
-    public ExecutionResult<MessageResponse> executeRequest() {
-        List<Map> accounts = Base.findAll(AJEntityUserAccounts.SELECT_ACCOUNTS_BY_EMAIL, email);
-        JsonArray accountsAsJsonArray = new JsonArray(accounts);
-        final JsonObject response = new JsonObject();
-        response.put("user_accounts", accountsAsJsonArray);
-        return new ExecutionResult<>(MessageResponseFactory.createPostResponse(response),
-            ExecutionResult.ExecutionStatus.SUCCESSFUL);
-    }
-
-    @Override
-    public boolean handlerReadOnly() {
-        return false;
-    }
+  @Override
+  public boolean handlerReadOnly() {
+    return false;
+  }
 
 }
--- src/main/java/org/gooru/nucleus/auth/handlers/processors/repositories/activejdbc/entities/AJEntityUserAccounts.java
+++ src/main/java/org/gooru/nucleus/auth/handlers/processors/repositories/activejdbc/entities/AJEntityUserAccounts.java
@@ -16,42 +16,38 @@ import org.javalite.activejdbc.Model;
 
 public class AJEntityUserAccounts extends Model {
 
-	public static final String EMAIL = "email";
+  private static final Set<String> USERS_ACCOUNTS_PAYLOAD_FIELDS =
+      new HashSet<>(Arrays.asList(ParameterConstants.PARAM_EMAIL));
 
-	private static final Set<String> USERS_ACCOUNTS_PAYLOAD_FIELDS = new HashSet<>(
-			Arrays.asList(ParameterConstants.PARAM_EMAIL, EMAIL));
+  public static final String SELECT_ACCOUNTS_BY_EMAIL =
+      "select tenant.name as tenant_name, tenant.image_url, users.login_type, tenant.short_name as tenant_short_name from users "
+          + "inner join tenant on tenant.id = users.tenant_id where email= ?";
 
-	public static final List<String> USER_ACCOUNTS_FIELDS = Arrays.asList("name", "image_url", "login_type",
-			"short_name");
+  private static final Map<String, FieldValidator> validatorRegistry;
 
-	public static final String SELECT_ACCOUNTS_BY_EMAIL = "select tenant.name as tenant_name, tenant.image_url, users.login_type, tenant.short_name as tenant_short_name from users "
-			+ "inner join tenant on tenant.id = users.tenant_id where email= ?";
+  static {
+    validatorRegistry = initializeValidators();
+  }
 
-	private static final Map<String, FieldValidator> validatorRegistry;
+  private static class UserAccountsValidationRegistry implements ValidatorRegistry {
+    @Override
+    public FieldValidator lookupValidator(String fieldName) {
+      return validatorRegistry.get(fieldName);
+    }
+  }
 
-	static {
-		validatorRegistry = initializeValidators();
-	}
+  public static ValidatorRegistry getValidatorRegistry() {
+    return new UserAccountsValidationRegistry();
+  }
 
-	private static class UserAccountsValidationRegistry implements ValidatorRegistry {
-		@Override
-		public FieldValidator lookupValidator(String fieldName) {
-			return validatorRegistry.get(fieldName);
-		}
-	}
+  private static Map<String, FieldValidator> initializeValidators() {
+    Map<String, FieldValidator> validatorMap = new HashMap<>();
+    validatorMap.put(ParameterConstants.PARAM_EMAIL, (FieldValidator::validateEmail));
+    return validatorMap;
+  }
 
-	public static ValidatorRegistry getValidatorRegistry() {
-		return new UserAccountsValidationRegistry();
-	}
-
-	private static Map<String, FieldValidator> initializeValidators() {
-		Map<String, FieldValidator> validatorMap = new HashMap<>();
-		validatorMap.put(EMAIL, (FieldValidator::validateEmail));
-		return validatorMap;
-	}
-
-	public static FieldSelector usersAccountsFieldSelector() {
-		return () -> Collections.unmodifiableSet(USERS_ACCOUNTS_PAYLOAD_FIELDS);
-	}
+  public static FieldSelector mandatoryFields() {
+    return () -> Collections.unmodifiableSet(USERS_ACCOUNTS_PAYLOAD_FIELDS);
+  }
 
 }
